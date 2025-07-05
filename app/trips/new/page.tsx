03913cf0ed993bc43d661555e7a41a2365893883
ausdin567567 <rahmanausdin@gmail.com>
2025-07-05 14:18:32 -0400

    Creating the dashboard, my trips mostly finished

--- app/trips/new/page.tsx
+++ app/trips/new/page.tsx
@@ -2,14 +2,30 @@
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { createTrip } from "@/lib/actions/create-trip"
+import { UploadButton } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
+import Image from "next/image"
+import { useState, useTransition } from "react"
 
 export default function NewTrip(){
+    const [isPending, startTransition] = useTransition();
+    const [imageUrl, setImageUrl] = useState<string | null>(null)
     return <div className="max-w-lg mx-auto mt-10">
         <Card>
             <CardHeader>New Trip</CardHeader>
             <CardContent>
-                <form className="space-y-6">
+                <form 
+                className="space-y-6" 
+                action={(formData: FormData) => {
+
+                    if (imageUrl) {
+                        formData.append("imageUrl", imageUrl)
+                    }
+                    startTransition(() => {
+                        createTrip(formData);
+                    })
+                }}>
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-1">
                             {" "}
@@ -72,8 +88,33 @@ export default function NewTrip(){
                         </div>
                     </div>
 
-                    <Button type="submit" className="w-full">
-                        Create Trip
+                    <div>
+                        <label>Trip Image</label>
+
+                        {imageUrl && (
+                            <Image
+                                src={imageUrl}
+                                alt="Trip Preview"
+                                className="w-full mb-4 rounded-md max-h-48 object-cover"
+                                width={300}
+                                height= {100}
+                            />
+                        )}
+                        <UploadButton
+                            endpoint="imageUploader"
+                            onClientUploadComplete={(res) => {
+                                if (res && res[0].ufsUrl){
+                                    setImageUrl(res[0].ufsUrl)
+                                }
+                            }}
+
+                            onUploadError={(error) => {
+                                console.error("Upload error: ", error);
+                            }}
+                        />
+                    </div>
+                    <Button type="submit" disabled={isPending} className="w-full">
+                        {isPending ? "Creating..." : "Create Trip"}
                     </Button>
                 </form>
             </CardContent>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns: [
+      {
+      hostname: "2f5e2oje7i.ufs.sh",
+    },
+  ]
+  }
 };
 
 export default nextConfig;
