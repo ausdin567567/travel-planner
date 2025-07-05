b61131ae07ba6d12e34d413b932cc78f8769c889
ausdin567567 <rahmanausdin@gmail.com>
2025-07-04 22:52:04 -0400

    Tried to fix the code to fix the OAuth issue

--- auth.ts
+++ auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
-import {PrismaAdapter} from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma";
 
-export const {auth, handlers, signIn, signOut} = NextAuth({
+export const { auth, handlers, signIn, signOut } = NextAuth({
     providers: [GitHub],
-    adapter: PrismaAdapter(prisma)
+    adapter: PrismaAdapter(prisma),
 })
